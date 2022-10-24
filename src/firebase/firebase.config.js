e20a615fafb319784c341f2d238f6d64cd92172a
razan111 <razan15-12966@diu.edu.bd>
2022-10-25 00:58:53 +0600

    needed some equepments install for this project

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAlXlpAtoxtDTj0w2afxa_Dz0BSPuHHJqg",
+  authDomain: "kindergarten-3a34b.firebaseapp.com",
+  projectId: "kindergarten-3a34b",
+  storageBucket: "kindergarten-3a34b.appspot.com",
+  messagingSenderId: "1054103721530",
+  appId: "1:1054103721530:web:1ab4427c3a32290ba24703"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
