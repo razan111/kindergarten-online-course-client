8fbcc322dc52960133000cfa9d8d7f7d94f54c04
razan111 <razan15-12966@diu.edu.bd>
2022-10-25 21:16:01 +0600

    Courses list added in the leftNavSide

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftSideNav = () => {
+
+    const [cetagories, setCetagories] = useState([])
+
+    useEffect(() =>{
+        fetch('http://localhost:5000/course-catagories')
+        .then(res => res.json())
+        .then(data => setCetagories(data))
+    }, [])
+
+
     return (
         <div>
-            <h2>Left side navbar</h2>
+            <h2>All Cetagory: {cetagories.length}</h2>
+
+            <div>
+                {
+                    cetagories.map(cetagory => <p key={cetagory.id}>
+                        <Link>{cetagory.name}</Link>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../Pages/Shared/Header/Header';
+import LeftSideNav from '../Pages/Shared/LeftSideNav/LeftSideNav';
 
 const Main = () => {
     return (
         <div>
             <Header></Header>
-            <Outlet></Outlet>
-            
+
+
+
+            <section className="text-gray-600 body-font">
+                <div className="container px-5 py-24 mx-auto flex flex-wrap">
+                    <div className='md:w-1/5'>
+                        <LeftSideNav></LeftSideNav>
+                    </div>
+                    <div className="md:w-4/5 md:pl-6">
+                        
+                    <Outlet></Outlet>
+                    </div>
+                </div>
+            </section>
+
+
+
+
+
+           
+
         </div>
     );
 };
