8671bf8a9c28bd775a90239afee9b8942aa8e2b4
razan111 <razan15-12966@diu.edu.bd>
2022-10-27 19:03:55 +0600

    header update

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -10,22 +10,22 @@ const Home = () => {
     return (
         <div >
 
-            <div className="carousel w-full">
-                <div id="slide1" className="carousel-item relative w-full h-96">
+            <div className="carousel w-full" style={{height: "80vh"}}>
+                <div id="slide1" className="carousel-item relative w-full">
                     <img className='object-cover' src={img1} className="w-full" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide4" className="btn btn-circle">❮</a>
                         <a href="#slide2" className="btn btn-circle">❯</a>
                     </div>
                 </div>
-                <div id="slide2" className="carousel-item relative w-full h-96">
+                <div id="slide2" className="carousel-item relative w-full ">
                     <img src={img2} className="w-full" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide1" className="btn btn-circle">❮</a>
                         <a href="#slide3" className="btn btn-circle">❯</a>
                     </div>
                 </div>
-                <div id="slide3" className="carousel-item relative w-full h-96">
+                <div id="slide3" className="carousel-item relative w-full">
                     <img src={img3} className="w-full" />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide2" className="btn btn-circle">❮</a>
